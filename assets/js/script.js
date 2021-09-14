eee350ccd551916375cce21f3f479d86afc61679
jl118 <jen.liebelt@gmail.com>
2021-09-14 15:48:05 -0400

    added initial js and images

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// declared variables
+var time = moment();
+var cityInput = document.querySelector('#city-input');
+var searchBtn = document.querySelector('#search-btn');
+var cityNameEl = document.querySelector('#city-name');
+var cityArray = [];
+var apiKey = '5f3d8e61f6ecd062e3ea0bfd4154ce1e';
+
